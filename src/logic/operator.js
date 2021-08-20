4b84b18d6e69be9fac8def3d42fa5ccf721d0e05
Mwila Kaunda <mbkcust@gmail.com>
2021-08-20 12:14:53 +0200

    Fix linter error

--- src/logic/operator.js
+++ src/logic/operator.js
@@ -16,7 +16,7 @@ export default function operate(numberOne, numberTwo, operation) {
     if (numberTwo === '0') {
       return 'Can\'t divide by 0';
     }
-    
+
     return one.div(two).toString();
   }
   if (operation === '%') {
